6c64b7dfd0a28b1a7103c88fe1e6aca9f2aa1a90
lesguillemets <kgkacko+github@gmail.com>
2024-12-23 01:42:33 +0900

    split into functions

--- src/person.ts
+++ src/person.ts
@@ -30,11 +30,31 @@ export enum AssignmentAC {
 	Regular = "regular",
 }
 
+function posToAssignmentAC(pos: Position): AssignmentAC {
+	if (pos === Position.Chief) {
+		return AssignmentAC.BiWeekly;
+	}
+	if (pos === Position.FullTime) {
+		return AssignmentAC.Regular;
+	}
+	return AssignmentAC.None;
+}
+
 // E を割り当てられる頻度
 export enum AssignmentE {
 	None = "none",
 	Half = "once in two cours",
 	Regular = "regular",
+}
+
+function posToAssignmentE(pos: Position): AssignmentE {
+	if (p === Position.Chief) {
+		return AssignmentE.Half;
+	}
+	if (p === Position.FullTime || p === Position.FixedTerm) {
+		return AssignmentE.Regular;
+	}
+	return AssignmentE.None;
 }
 
 export class Person {
@@ -70,26 +90,14 @@ export class Person {
 
 		// AssignmentAC が特に指定されなければ役職に従う
 		if (aac === undefined) {
-			if (p === Position.Chief) {
-				this.assignAC = AssignmentAC.BiWeekly;
-			} else if (p === Position.FullTime) {
-				this.assignAC = AssignmentAC.Regular;
-			} else {
-				this.assignAC = AssignmentAC.None;
-			}
+			this.assignAC = posToAssignmentAC(p);
 		} else {
 			this.assignAC = aac;
 		}
 
 		// AssignmentE が特に指定されなければ役職に従う
 		if (ae === undefined) {
-			if (p === Position.Chief) {
-				this.assignE = AssignmentE.Half;
-			} else if (p === Position.FullTime || p === Position.FixedTerm) {
-				this.assignE = AssignmentE.Regular;
-			} else {
-				this.assignE = AssignmentE.None;
-			}
+			this.assignE = posToAssignmentE(p);
 		} else {
 			this.assignE = ae;
 		}
