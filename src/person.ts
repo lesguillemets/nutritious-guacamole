3b755af9011299784f420a58960ef575200bb542
lesguillemets <kgkacko+github@gmail.com>
2024-12-23 01:08:49 +0900

    assignment to ac and e

--- src/person.ts
+++ src/person.ts
@@ -20,20 +20,72 @@ export enum DayofWeek {
 	Sat = 6,
 }
 
+// A/C を割り当てられる頻度
+export enum AssignmentAC {
+	None = "none",
+	BiWeekly = "biweekly",
+	Regular = "regular",
+}
+
+// E を割り当てられる頻度
+export enum AssignmentE {
+	None = "none",
+	Half = "once in two cours",
+	Regular = "regular",
+}
+
 export class Person {
 	// お名前，かつこれで区別するので一意であってほしい
 	name: string;
 	position: Position; // 役職
-	// FIXME
+	// FIXME: 多分 [DayofWeek, number | asterisk] みたいな感じで週を指定したらいい
 	/** 外来日…なのだが隔週月曜は別に扱う */
 	weeklyOutP: DayofWeek | null;
 	/** 平日のシフト履歴 */
 	weekDayHistory: [Date, WeekDayDuty][] = new Array();
 	/** 休日のシフト履歴 */
 	weekEndHistory: [Date, WeekEndDuty][] = new Array();
-	constructor(name: string, p: Position, w: DayofWeek | null = null) {
+
+	// A/C を割り当てられる頻度
+	assignAC: AssignmentAC;
+	/** E を割り当てられる頻度 */
+	assignE: AssignmentE;
+
+	constructor(
+		name: string,
+		p: Position,
+		w: DayofWeek | null = null,
+		aac?: AssignmentAC,
+		ae?: AssignmentE,
+	) {
 		this.name = name;
 		this.position = p;
 		this.weeklyOutP = w;
+
+		// AssignmentAC が特に指定されなければ役職に従う
+		if (aac === undefined) {
+			if (p === Position.Chief) {
+				this.assignAC = AssignmentAC.BiWeekly;
+			} else if (p === Position.FullTime) {
+				this.assignAC = AssignmentAC.Regular;
+			} else {
+				this.assignAC = AssignmentAC.None;
+			}
+		} else {
+			this.assignAC = aac;
+		}
+
+		// AssignmentE が特に指定されなければ役職に従う
+		if (ae === undefined) {
+			if (p === Position.Chief) {
+				this.assignE = AssignmentE.Half;
+			} else if (p === Position.FullTime || p === Position.FixedTerm) {
+				this.assignE = AssignmentE.Regular;
+			} else {
+				this.assignE = AssignmentE.None;
+			}
+		} else {
+			this.assignE = ae;
+		}
 	}
 }
