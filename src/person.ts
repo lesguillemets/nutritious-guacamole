dae00a75af5952b9303146d4bdfa827e042ecc6d
lesguillemets <kgkacko+github@gmail.com>
2024-12-23 14:51:15 +0900

    better handling for optpatient days

--- src/person.ts
+++ src/person.ts
@@ -20,6 +20,39 @@ export enum DayofWeek {
 	Sat = 6,
 }
 
+/** 外来業務 */
+export class OutPDays {
+	// [曜日, [第n週]] (1-indexed) という形式にした．
+	// FIXME: 0-indexed のほうが統一性がある？
+	// 例えば毎週月曜・1・3週木曜なら
+	// [[DayofWeek.Mon, null], [DayofWeek.Thu, [1,3]]
+	slots: [DayofWeek, number[] | null][];
+
+	constructor(d: [DayofWeek, number[] | null][] = []) {
+		this.slots = d;
+	}
+
+	/** その日が外来日かどうか */
+	is(d: Date): boolean {
+		for (const wd of this.slots) {
+			// 曜日として記録があって…
+			if (d.getDay() === wd[0]) {
+				// 週の限定がなくてその曜日が入ってたら true
+				if (wd[1] === null) {
+					return true;
+				}
+				// 第何週目のその曜日か
+				const day = d.getDate();
+				const nth = Math.floor(day / 7) + (day % 7 === 0 ? 0 : 1);
+				// 週の限定がある場合はそっちを見る，
+				return wd[1].includes(nth);
+			}
+		}
+		// 該当の曜日がない
+		return false;
+	}
+}
+
 // 個々人の予定表．
 type PersonalCalendar = [Date, WeekDayDuty | WeekEndDuty][];
 
@@ -63,7 +96,7 @@ export class Person {
 	position: Position; // 役職
 	// FIXME: 多分 [DayofWeek, number | asterisk] みたいな感じで週を指定したらいい
 	/** 外来日…なのだが隔週月曜は別に扱う */
-	weeklyOutP: DayofWeek | null;
+	outPDays: OutPDays;
 
 	/** シフト履歴，とりあえずクールごとにまとめたりはしない方針で */
 	history: PersonalCalendar[] = new Array();
@@ -83,14 +116,14 @@ export class Person {
 	constructor(
 		name: string,
 		p: Position,
-		w: DayofWeek | null = null,
+		w: OutPDays = new OutPDays(),
 		ward: string | null = null,
 		aac?: AssignmentAC,
 		ae?: AssignmentE,
 	) {
 		this.name = name;
 		this.position = p;
-		this.weeklyOutP = w;
+		this.outPDays = w;
 		this.ward = ward;
 
 		// AssignmentAC が特に指定されなければ役職に従う
